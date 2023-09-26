ab7855c5a2da58740dbfc5b8014fd82a556bf6ad
hyunbaekim <hyunbae9242@paymint.co.kr>
2023-09-26 20:24:56 +0900

    산술연산

--- CShap/Program.cs
+++ CShap/Program.cs
@@ -1,28 +1,22 @@
-﻿using System;
-
-namespace MyApp
+﻿namespace MyApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // string -> int
-
-            //string input = Console.ReadLine();
-            //int number = int.Parse(input);
-            //Console.WriteLine(input);
-
-            // int -> string
-            int maxHp = 200;
-            int hp = 100;
-
-            // 당신의 hp는 ?? 입니다.
-
-            // string message = string.Format("당신의 HP는 {0}/{1}입니다.", hp, maxHp);
-            // Console.WriteLine(message);
-
-            string message = $"당신의 HP는 {hp} / {maxHp}입니다.";
-            Console.WriteLine(message);
+            int a = 10;
+            // a = a + 5;
+            a += 5;
+            // a = a - 5;
+            a -= 5;
+            // a = a * 5;
+            a *= 5;
+            // a = a / 5;
+            a /= 5;
+            // a += 1;
+            a++;
+            // a -= 1;
+            a--;
         }
     }
 }
