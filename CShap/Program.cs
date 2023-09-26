e63761db67dc7b9377a707656e6280a62cf0c7c4
hyunbaekim <hyunbae9242@paymint.co.kr>
2023-09-26 17:54:34 +0900

    init

---
+++ CShap/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string -> int
+
+            //string input = Console.ReadLine();
+            //int number = int.Parse(input);
+            //Console.WriteLine(input);
+
+            // int -> string
+            int maxHp = 200;
+            int hp = 100;
+
+            // 당신의 hp는 ?? 입니다.
+
+            string message = string.Format("당신의 HP는 {0}/{1}입니다.", hp, maxHp);
+            Console.WriteLine(message);
+        }
+    }
+}
